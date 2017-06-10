35fe48362bf17ca11d75114590d74d0a15b05959
de.gautam19@gmail.com <de.gautam19@gmail.com>
2017-06-10 15:06:43 +0530

    method Addition

--- src/com/P1/ExcelRead.java
+++ src/com/P1/ExcelRead.java
@@ -3,6 +3,13 @@ package com.P1;
 public class ExcelRead {
 	public static void main(String[] args) {
 		System.out.println("Hello World! GitHub Project");
+		
+	}
+	
+	public void readExcel(String fileName){
+		
+		
+		
 	}
 
 }
