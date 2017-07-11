bcebc53759ef673457478018b2987e2202c4f155
de.gautam19@gmail.com <de.gautam19@gmail.com>
2017-07-11 22:08:52 +0530

    User1toNewBranch

--- src/com/P1/TextRead.java
+++ src/com/P1/TextRead.java
@@ -3,25 +3,21 @@ package com.P1;
 public class TextRead {
 	static int i = 5;
 
-	public static int read(){
+	public static void read(){
 		try {
-			int k=i/2;
-			return k;
+			System.out.println("Test");
 			
 		} catch (Exception e) {
-			return i;
-			// TODO: handle exception
+	
 		}
 		finally {
-			System.out.println("Finally");
-			return 9;
 		}
 		
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("User1 TextRead");
-		System.out.println(read());
+		read();
 	}
 
 }
