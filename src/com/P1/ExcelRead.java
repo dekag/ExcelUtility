afaac8783bacb45d35389e31ff27f182b19fd0c6
de.gautam19@gmail.com <de.gautam19@gmail.com>
2017-07-11 21:56:16 +0530

    User1Commit

--- src/com/P1/ExcelRead.java
+++ src/com/P1/ExcelRead.java
@@ -4,17 +4,18 @@ public class ExcelRead {
 	
 	public static String fileName = "test";
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Hello World! GitHub Project");
 		ExcelRead obj = new ExcelRead();
 		obj.readExcel(fileName);
 		
 	}
 	
-	public void readExcel(String fileName){
+	public void readExcel(String fileName) throws InterruptedException{
 		
 		//Added new sysout
 		System.out.println("User1 Commit");
+		Thread.sleep(5000);
 		System.out.println("User1 Change without pulling");
 		System.out.println("User2 Commit");
 		//Sysout from User 2
