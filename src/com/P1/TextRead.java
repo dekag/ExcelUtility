d2c92a26c4fa43fb74c62b8c64e1c25572ceca90
de.gautam19@gmail.com <de.gautam19@gmail.com>
2017-08-17 22:53:37 +0530

    FileCommit

--- src/com/P1/TextRead.java
+++ src/com/P1/TextRead.java
@@ -16,6 +16,7 @@ public class TextRead {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("Added one new comment");
 		System.out.println("User1 TextRead");
 		read();
 		System.out.println("User1Commented");
