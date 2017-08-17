c47ee48e4018a984f2244da75d711bc908445b53
de.gautam19@gmail.com <de.gautam19@gmail.com>
2017-08-18 00:51:04 +0530

    MergeComplete

--- src/com/P1/ExcelRead.java
+++ src/com/P1/ExcelRead.java
@@ -2,8 +2,14 @@ package com.P1;
 
 public class ExcelRead {
 	public void projectTwo() {
+		System.out.println("Add Print ProjectOne");
 		System.out.println("Add Print ProjectTwo");
 		System.out.println("Add Print ProjectTwo");
-		
+		System.out.println("Add Print ProjectOne");
+	}
+	public void projectOne() {
+		System.out.println("Add Print ProjectOne");
+		System.out.println("Add Print ProjectTwo");
+		System.out.println("Add Print ProjectOne");
 	}
 }
--- src/com/P1/TextRead.java
+++ src/com/P1/TextRead.java
@@ -8,9 +8,10 @@ public class TextRead {
 	public void projectTwo() {
 		System.out.println("Added one print ProjectTwo");
 		System.out.println("Added one print ProjectTwo");
+		System.out.println("Added one print ProjectOne");
 	}
 	public void projectTwo(String methodOne) {
-
+		System.out.println("Added one print ProjectOne");
 	}
 	public void projectTwo(int methodTwo) {
 
