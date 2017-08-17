a19272f0703b0364c5e13df3c756ebba0cbdfebc
de.gautam19@gmail.com <de.gautam19@gmail.com>
2017-08-18 00:08:03 +0530

    removeAll

--- src/com/P1/Git1Dir.java
+++ src/com/P1/Git1Dir.java
@@ -5,15 +5,5 @@ public class Git1Dir {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-	public void addOne(){
-		System.out.println();
-	}
-	public void addThree(){
-		System.out.println();
-	}
-	public void addFour(){
-		System.out.println();
-	}
-
+	}	
 }
