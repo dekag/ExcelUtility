a28eab712a832b3819182a4210add058180ee1f0
de.gautam19@gmail.com <de.gautam19@gmail.com>
2017-08-18 00:43:33 +0530

    Project2Added

--- src/com/P1/ExcelRead.java
+++ src/com/P1/ExcelRead.java
@@ -2,6 +2,6 @@ package com.P1;
 
 public class ExcelRead {
 	public void projectTwo() {
-
+		System.out.println("Add Print ProjectTwo");
 	}
 }
--- src/com/P1/TextRead.java
+++ src/com/P1/TextRead.java
@@ -2,9 +2,13 @@ package com.P1;
 
 public class TextRead {
 
-	public void projectOne(){
-}
-	public void projectTwo(){
-		
+	public void projectOne() {
+	}
+
+	public void projectTwo() {
+		System.out.println("Added one print ProjectTwo");
+	}
+	public void projectTwo(String methodOne) {
+
 	}
 }
