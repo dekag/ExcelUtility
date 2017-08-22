9d151fb81cac1b41570bf1f710260603759c9323
de.gautam19@gmail.com <de.gautam19@gmail.com>
2017-08-23 00:42:20 +0530

    Commit

--- src/com/P1/ExcelRead.java
+++ src/com/P1/ExcelRead.java
@@ -1,7 +1,12 @@
 package com.P1;
 
 public class ExcelRead {
+	
+	public void test(){
+		System.out.println("Main Project");
+	}
+	
 	public static void main(String[] args) {
-
+		System.out.println("TestMethod");
 	}
 }
